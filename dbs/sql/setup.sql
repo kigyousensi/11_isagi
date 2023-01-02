615f25acffd39eb9e24d69cef438409f683bfe66
katsumi <adgjmptw@mineo.jp>
2023-01-02 13:28:38 +0900

    SQL修正,メーカー追加

--- dbs/sql/setup.sql
+++ dbs/sql/setup.sql
@@ -5,55 +5,43 @@ grant create,select,update,delete,insert,alter on `accountdb`.* to isagi;
 create database `assetdb`;
 grant create,select,update,delete,insert,alter on `assetdb`.* to isagi;
 
-CREATE TABLE IF NOT EXISTS account(
-usid char(16) primary key,
-token char(32),
-name char(16),
-mail VARBINARY(120),
+CREATE TABLE IF NOT EXISTS accountdb.account(
+usid char(12) primary key,
+token char(24),
+name varchar(16),
+mail VARBINARY(64),
 pass char(255),
 state bit(1));
 
-CREATE TABLE IF NOT EXISTS account_count(
-grptyp TINYINT UNSIGNED,
-grpcnt MEDIUMINT UNSIGNED,
-grpof TINYINT UNSIGNED,
-accnt MEDIUMINT UNSIGNED,
-gn varchar(32));
+CREATE TABLE IF NOT EXISTS accountdb.account_count(
+ustyp char(1),
+usof TINYINT UNSIGNED,
+usno MEDIUMINT UNSIGNED);
 
-CREATE TABLE IF NOT EXISTS sns(
-usid char(16),
-snsid char(4),
+
+CREATE TABLE IF NOT EXISTS accountdb.corp(
+usid char(6) primary key,
+id MEDIUMINT  UNSIGNED,
+store SMALLINT UNSIGNED,
+pass char(255),
+mail VARBINARY(64),
+name varchar(12),
+rignt TINYINT UNSIGNED,
 state bit(1));
 
-CREATE TABLE IF NOT EXISTS asset(
-asid char(11) primary key,
-regist DATETIME,
-type varchar(16),
-corp CHAR(32),
-model CHAR(32),
-sup CHAR(32),
-serial CHAR(32),
-bouhan CHAR(32),
-memo varchar(64),
-icorp varchar(24),
-ino char(32),
-idate DATETIME,
-itel1 char(16),
-itel2 char(16),
-release bit(1),
-repsw bit(1),
-repdate DATETIME,
-repno char(10),
-proof bit(1),
+CREATE TABLE IF NOT EXISTS accountdb.corp_count(
+cotyp char(1),
+coof SMALLINT UNSIGNED);
+
+CREATE TABLE IF NOT EXISTS accountdb.corp_prof(
+coid char(6),
+name varchar(32),
+area SMALLINT UNSIGNED,
+uno MEDIUMINT UNSIGNED,
 state bit(1));
 
-CREATE TABLE IF NOT EXISTS touki(
-rid INT UNSIGNED primary key AUTO_INCREMENT,
-usid INT UNSIGNED,
-asid char(11),
-ouid INT UNSIGNED,
-pin char(6));
+CREATE TABLE IF NOT EXISTS accountdb.store_d(
+coid char(6),
+stid SMALLINT UNSIGNED,
+name varchar(16));
 
-CREATE TABLE IF NOT EXISTS assetdb.asset_count(
-cnt INT UNSIGNED,
-alph char(1));
--- docker/start-inst-dbs.sh
+++ docker/start-inst-dbs.sh
@@ -8,12 +8,14 @@ CONFD="${BASE}/dbs/conf/"
 SQLD="${BASE}/dbs/sql/"
 
 ## main
+## SQLDにあるsqlファイルは自動で実行される
 docker run -p 3306:3306 --network isagi-net --name dbs\
  -v "${DBDIR}:/var/lib/mysqlbackup"\
  -v "${CONFD}:/etc/mysql/conf.d" \
  -v "${SQLD}:/docker-entrypoint-initdb.d" \
  -e MYSQL_ROOT_PASSWORD=mysql -d mysql 
 
- docker ps | grep dbs
-#--log-driver=syslog \
+
+docker ps | grep dbs
+#--log-driver=syslog \  うまく動作しないときはこのオプションを付けてsyslogにメッセージを出す
 
